90d4c0fc11ab0a8bbd90bf3f757a34e4e404cf7e
eyoa <yoaevelyn@gmail.com>
2021-05-23 19:15:32 -0400

    add bootstrap and write Nav component

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,8 +1,17 @@
+import { Nav, Navbar } from 'react-bootstrap';
+
 export const Navigation = () => {
   return (
-    <nav>
-      <span class='nav--text'>Project Name</span>
-      <div>nav</div>
-    </nav>
+    <Navbar fixed='top' bg='light' expand='lg' classNmae='flex-column'>
+      <Navbar.Brand href='#'>ProjectName</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic=navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#home'>Map</Nav.Link>
+          <Nav.Link href='#home'>Trip</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
