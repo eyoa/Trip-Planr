c71bf815b3f10eca17ed957be2b40673cb2f9d48
eyoa <yoaevelyn@gmail.com>
2021-05-24 11:19:22 -0400

    add homepage, mapview and bootstrap icons

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,12 +1,41 @@
+import './Homepage.scss';
+import bgImg from './images/landingBg.jpg';
 import { Fragment } from 'react';
+import { FormControl, Container, Row, Col, InputGroup } from 'react-bootstrap';
+import { Search as SearchIcon } from 'react-bootstrap-icons';
 
 export const Homepage = () => {
   return (
     <Fragment>
-      <div>
-        <h1>Homepage</h1>
-        <h1>Homepage</h1>
-        <h1>Homepage</h1>
+      <div
+        className='landing-wrapper'
+        style={{
+          backgroundImage: `url(${bgImg})`
+        }}
+      >
+        <Container fluid='true'>
+          <Row className='spacer'></Row>
+          <Row className='justify-content-md-center'>
+            <Col sm='true'>
+              <InputGroup>
+                <InputGroup className='mb-3'>
+                  <FormControl
+                    size='lg'
+                    className='search'
+                    placeholder='Search'
+                    aria-label='Search'
+                    aria-describedby='Search'
+                  />
+                  <InputGroup.Append>
+                    <InputGroup.Text id='basic-addon2' className='search'>
+                      <SearchIcon />
+                    </InputGroup.Text>
+                  </InputGroup.Append>
+                </InputGroup>
+              </InputGroup>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </Fragment>
   );
--- frontend/src/components/Mapview.js
+++ frontend/src/components/Mapview.js
@@ -1,13 +1,15 @@
 import { Fragment } from 'react';
+import bgImg from './images/placeholder_map.png';
 
 export const Mapview = () => {
   return (
     <Fragment>
-      <div>
-        <h1>Mapview</h1>
-        <h1>Mapview</h1>
-        <h1>Mapview</h1>
-        <h1>Mapview</h1>
+      <div
+        className='landing-wrapper'
+        style={{
+          backgroundImage: `url(${bgImg})`
+        }}
+      >
         <h1>Mapview</h1>
       </div>
     </Fragment>
