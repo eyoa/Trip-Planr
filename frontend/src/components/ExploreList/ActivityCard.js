e31d46be9853e1d3343e242a7dcaa752549a15c5
eyoa <yoaevelyn@gmail.com>
2021-06-02 17:06:37 -0400

    tweaked activity card css

--- frontend/src/components/ExploreList/ActivityCard.js
+++ frontend/src/components/ExploreList/ActivityCard.js
@@ -20,34 +20,35 @@ export const ActivityCard = (props) => {
   return (
     <>
       {name && description && (
-        <Accordion>
-          <Card className='activity-card'>
-            <Card.Img
+        <Card
+          className='activity-card'
+          style={{
+            backgroundImage: `url(${img_url})`,
+            backgroundSize: 'cover'
+          }}
+        >
+          {/* <Card.Img
               variant='top'
               src={img_url}
               alt='image'
               className='card-img'
-            />
-            <Card.ImgOverlay>
-              <aside className='rating'>{rating}</aside>
-              <aside className='add' onClick={addEntryToTrip}>
-                <PlusSquareFill />
-              </aside>
-              <aside className='suggest'>
-                <PlusSquareFill onClick={suggestActivity} />
-              </aside>
-            </Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{name}</Card.Title>
-            </Card.Body>
-            <Accordion.Toggle as={Button} variant='secondary' eventKey='exp'>
-              More Click!
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey='exp'>
-              <Card.Text>{description}</Card.Text>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+            /> */}
+          <aside className='rating'>{rating}</aside>
+          <aside className='add' onClick={addEntryToTrip}>
+            <PlusSquareFill />
+          </aside>
+          <aside className='suggest'>
+            <PlusSquareFill onClick={suggestActivity} />
+          </aside>
+          <h2 className='title'>{name}</h2>
+          <span className='description'>{description}</span>
+          {/* <Accordion.Toggle as={Button} variant='secondary' eventKey='exp'>
+            More Click!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey='exp'>
+            <Card.Text>{description}</Card.Text>
+          </Accordion.Collapse> */}
+        </Card>
       )}
     </>
   );
